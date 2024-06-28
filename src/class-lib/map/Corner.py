eceb9f679cb68baa36476c8758a95f34fd378686
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-06-28 17:19:30 +0200

    Creat Map with tiles, corners and edges

--- src/class-lib/map/Corner.py
+++ src/class-lib/map/Corner.py
@@ -2,5 +2,21 @@ class Corner:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.edge = [] # x3 max - x2 min
+        self.edges = [] # x3 max - x2 min
         self.tiles = [] # x2 max - x1 min
+    
+    def __eq__(self, other):
+        if isinstance(other, Corner):
+            return self.x == other.x and self.y == other.y
+        return False
+    
+    def __lt__(self, other):
+        if isinstance(other, Corner):
+            if self.x < other.x:
+                return True
+            elif self.x == other.x:
+                return self.y < other.y
+        return False
+    
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y})"
