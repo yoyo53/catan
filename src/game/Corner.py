c53e961a68dece8f65860a9dfd8c92a05c974e4e
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-07-01 16:43:04 +0200

    merge gamelogic with map 2

--- src/game/Corner.py
+++
@@ -1,6 +0,0 @@
-class Corner:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.edge = []
-        self.tiles = []
--- src/game/Edge.py
+++
@@ -1,7 +0,0 @@
-class Edge:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.corners = []
-        self.tiles = []
-        self.road = None
