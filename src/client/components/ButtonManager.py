693eb3461eacf631b4281ef237ff76ff6fcf46bf
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-07-06 18:00:43 +0200

    enlever les 600 boutons crée lors du main menu

--- src/client/components/ButtonManager.py
+++ src/client/components/ButtonManager.py
@@ -17,10 +17,11 @@ class ButtonManager:
         self.buttons = []
 
     def create_main_menu_buttons(self):
-        create_lobby_button = Button(self.screen, self.colors.WHEAT, 100, 100, 250, 50, "Créer un lobby", self.colors.WHITE)
-        join_lobby_button = Button(self.screen, self.colors.ORE, 100, 200, 250, 50, "Rejoindre un lobby", self.colors.WHITE)
-        self.add_button(create_lobby_button)
-        self.add_button(join_lobby_button)
+        if len(self.buttons) == 0: # Only create buttons if there are none, ON CREEE PAS DES BOUTONS A CHAQUE FRAME HEIN LUCAS
+            create_lobby_button = Button(self.screen, self.colors.WHEAT, 100, 100, 250, 50, "Créer un lobby", self.colors.WHITE)
+            join_lobby_button = Button(self.screen, self.colors.ORE, 100, 200, 250, 50, "Rejoindre un lobby", self.colors.WHITE)
+            self.add_button(create_lobby_button)
+            self.add_button(join_lobby_button)
 
     def start_game_button(self):
         start_button = Button(self.screen, self.colors.WHEAT, self.screen.get_width() // 2, self.screen.get_height() // 2, 250, 50, "Lancer la partie", self.colors.WHITE)
