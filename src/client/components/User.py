02e6bf1eac82a96ba2dcd4817c7c50805fc4caa8
tonelomb <lucas.brancolini@efrei.net>
2024-07-09 15:49:42 +0200

    merge

--- src/client/components/User.py
+++ src/client/components/User.py
@@ -57,6 +57,7 @@ class User:
                     self.ui.display_error(error_message, previous_screen, previous_buttons)
                 case "game_start":
                     self.game = ClientGame(self.ui, response['data']['jsondata'])
+                    self.ui.draw_game()
                     self.ui.change_state("game_started")
                 case "turn_order":
                     self.game.turn_order = response['data']['turn_order']
